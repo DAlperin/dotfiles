113e59357247b0a43312e0a0daf163904e54efcd
Dov Alperin <dzalperin@gmail.com>
2021-11-22 01:16:21 +0000

    Install 1password with systemPackages instead of home manager to fix polkit

--- machines/nixosvm.nix
+++ machines/nixosvm.nix
@@ -43,6 +43,8 @@
     shell = pkgs.zsh;
   };
 
+  programs.zsh.enable = true;
+  environment.systemPackages = [ pkgs._1password-gui ];
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
