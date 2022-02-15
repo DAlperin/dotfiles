ee2dd669d304c031bf2bd2cf6926e5e6bc338dcb
Dov Alperin <git@dov.dev>
2022-02-15 13:46:22 -0500

    oops

--- users/worker/home.nix
+++ users/worker/home.nix
@@ -1,6 +1,6 @@
 { pkgs, config, ... }: {
   #Home manager configuration
-  home.username = "dovalperin";
+  home.username = "worker";
   home.homeDirectory = "/home/dovalperin";
   imports = [ ./../../home ./../../home/nixos ];
 }
